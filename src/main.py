1016ccda4ac5bd9343a5b6e6d40a1eac15398a03
janmilosic <milosic.jan@gmail.com>
2025-03-29 16:49:39 +0100

    added support for selecting skin region with mouse

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ import cv2 as cv
 import numpy as np
 import time
 
+risanje = False
+p_zacetek = (0, 0)
+p_konec = (0, 0)
+
 def zmanjsaj_sliko(slika, sirina, visina):
     return cv.resize(slika, (sirina, visina))
 
@@ -23,7 +27,37 @@ def prestej_piksle_z_barvo_koze(slika, barva_koze):
 def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze):
     pass
 
+def mouse_callback(event, x, y, flags, param):
+    global risanje, p_zacetek, p_konec
+    if event == cv.EVENT_LBUTTONDOWN:
+        risanje = True
+        p_zacetek = (x, y)
+    elif event == cv.EVENT_MOUSEMOVE and risanje:
+        p_konec = (x, y)
+    elif event == cv.EVENT_LBUTTONUP:
+        risanje = False
+        p_konec = (x, y)
+
 if __name__ == '__main__':
     kamera = cv.VideoCapture(0)
     ret, slika = kamera.read()
     slika = zmanjsaj_sliko(slika, 340, 220)
+
+    cv.namedWindow("Izberi obmocje koze")
+    cv.setMouseCallback("Izberi obmocje koze", mouse_callback)
+
+    # Uporabnik izbere obocje, kjer je koza
+    while True:
+        temp_slika = slika.copy()
+        if p_zacetek != p_konec:
+            cv.rectangle(temp_slika, p_zacetek, p_konec, (255, 0, 0), 2)
+        cv.imshow("Izberi obmocje koze", temp_slika)
+        if cv.waitKey(1) & 0xFF == ord('y'):
+            break
+
+    cv.destroyWindow("Izberi obmocje koze")
+
+    # Pretvori koordinati v urejen par
+    levo_zgoraj = (min(p_zacetek[0], p_konec[0]), min(p_zacetek[1], p_konec[1]))
+    desno_spodaj = (max(p_zacetek[0], p_konec[0]), max(p_zacetek[1], p_konec[1]))
+    barva_koze = doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj)

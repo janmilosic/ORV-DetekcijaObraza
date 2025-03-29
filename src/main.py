421480b1316c2ac55bd110eb18e86c4c0d9da438
janmilosic <milosic.jan@gmail.com>
2025-03-29 15:51:57 +0100

    added basic file structure and main functions

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print("Hello world!")
+import cv2 as cv
+import numpy as np
+import time
+
+def zmanjsaj_sliko(slika, sirina, visina):
+    pass
+
+def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj):
+    pass
+
+def prestej_piksle_z_barvo_koze(slika, barva_koze):
+    pass
+
+def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze):
+    pass
+
+if __name__ == '__main__':
+    pass

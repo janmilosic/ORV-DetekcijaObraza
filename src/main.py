79cef994c4edd1fb0e5f6368b847d694ad3f4b03
janmilosic <milosic.jan@gmail.com>
2025-03-29 16:46:10 +0100

    added function for skin color detection

--- src/main.py
+++ src/main.py
@@ -6,7 +6,16 @@ def zmanjsaj_sliko(slika, sirina, visina):
     return cv.resize(slika, (sirina, visina))
 
 def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj):
-    pass
+    x1, y1 = levo_zgoraj
+    x2, y2 = desno_spodaj
+    izrez = slika[y1:y2, x1:x2]
+    povprecje = np.mean(izrez, axis=(0, 1))  # Povprecna barva v obmocju
+    razpon = 40
+
+    # Meje koze
+    spodnja_meja = np.clip(povprecje - razpon, 0, 255).astype(np.uint8).reshape(1, 3)
+    zgornja_meja = np.clip(povprecje + razpon, 0, 255).astype(np.uint8).reshape(1, 3)
+    return spodnja_meja, zgornja_meja
 
 def prestej_piksle_z_barvo_koze(slika, barva_koze):
     pass
